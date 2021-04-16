263c99445a09a527533700ab75c7c89fbbc8ce9a
Marti D <me@martidolce.com>
2021-04-16 16:33:56 -0400

    Adding M/V/C for school_classes Adding M/V/C for students
    Signed-off-by: Marti D <me@martidolce.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :students, only: [:index, :show, :new, :edit, :create]
+  # get 'students/:id' => 'students#show'
+  # get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
+  resources :students, except: [:destroy, :index]
+  resources :school_classes, except: [:destroy, :index]
 end
